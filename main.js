dc70e405b709dc6674189c381d22ab680f3bf606
Olli Jokinen <olli.jokinen@futurice.com>
2017-06-26 16:41:14 +0300

    Add simpleForecastLatLon method

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const simpleForecast = require('./query/simpleForecast');
 const latestObservations = require('./query/latestObservations');
 
 module.exports = {
-	simpleForecast: simpleForecast.run,
-	latestObservations: latestObservations.run
+  simpleForecast: simpleForecast.run,
+  latestObservations: latestObservations.run,
+  simpleForecastLatLon: simpleForecast.runLatLon,
 };
